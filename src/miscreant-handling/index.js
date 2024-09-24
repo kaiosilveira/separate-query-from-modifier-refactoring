edbbdd53360657600506b626f03df31e671c4619
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-24 07:31:24 +0100

    remove return values from `alertForMiscreant`

--- src/miscreant-handling/index.js
+++ src/miscreant-handling/index.js
@@ -6,14 +6,11 @@ export function alertForMiscreant(people) {
   for (const p of people) {
     if (p === 'Don') {
       setOffAlarms();
-      return 'Don';
     }
     if (p === 'John') {
       setOffAlarms();
-      return 'John';
     }
   }
-  return '';
 }
 
 export function findMiscreant(people) {
--- src/miscreant-handling/index.test.js
+++ src/miscreant-handling/index.test.js
@@ -7,20 +7,17 @@ describe('alertForMiscreant', () => {
   beforeEach(() => spy.mockClear());
 
   it('should set off alarms for Don', () => {
-    const result = alertForMiscreant(['Don']);
-    expect(result).toBe('Don');
+    alertForMiscreant(['Don']);
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should set off alarms for John', () => {
-    const reuslt = alertForMiscreant(['John']);
-    expect(reuslt).toBe('John');
+    alertForMiscreant(['John']);
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should not set off alarms for other people', () => {
-    const result = alertForMiscreant(['Alice']);
-    expect(result).toBe('');
+    alertForMiscreant(['Alice']);
     expect(spy).not.toHaveBeenCalled();
   });
 });
